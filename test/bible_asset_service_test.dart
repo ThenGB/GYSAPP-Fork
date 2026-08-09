4ab435186b2a576ce074b62c05f793fed9950d8f
Gilbert <123263227+ThenGB@users.noreply.github.com>
2026-08-09 23:13:45 +0700

    Restore Bible repository test contract

--- test/bible_asset_service_test.dart
+++ test/bible_asset_service_test.dart
@@ -1,44 +1,79 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:church/data/services/asset_distribution/installed_asset_registry.dart';
-import 'package:church/data/services/asset_distribution/installed_asset_store.dart';
-import 'package:church/data/services/local_bible_asset_service.dart';
-import 'package:church/domain/entity/bible/bible_book_entity.dart';
-import 'package:church/domain/entity/bible/bible_verse_entity.dart';
-import 'package:church/domain/repository/bible_repository.dart';
-import 'package:dartz/dartz.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:sqflite_common/sqflite.dart';
+
+import 'package:church/data/services/asset_distribution/installed_asset_registry.dart';
+import 'package:church/data/services/asset_distribution/installed_asset_store.dart';
+import 'package:church/data/services/local_bible_asset_service.dart';
+import 'package:church/domain/entity/bible_book/bible_book.dart';
+import 'package:church/domain/entity/bible_ref/bible_ref.dart';
+import 'package:church/domain/entity/pericope/pericope.dart';
+import 'package:church/domain/entity/pericope_paralel/pericope_paralel.dart';
+import 'package:church/domain/entity/verse/verse.dart';
+import 'package:church/domain/repository/bible_repository.dart';
 
 class _FakeBibleRepository implements BibleRepository {
   @override
-  Future<Either<dynamic, List<BibleBook>>> getBooks(String version) async =>
-      const Right([]);
+  Future<List<BibleBook>> getBooks(Database db, {int? bookId}) async => [];
 
   @override
-  Future<Either<dynamic, List<BibleVerse>>> getVerses(
-    String version, {
+  Future<List<Verse>> getVerses(
+    Database db, {
     required int bookId,
     required int chapterId,
-  }) async => const Right([]);
+  }) async => [];
 
   @override
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+  Future<List<Pericope>> getPericope(
+    Database db, {
+    required int bookId,
+    required int chapterId,
+  }) async => [];
+
+  @override
+  Future<List<PericopeParalel>> getPericopeParalel(
+    Database db, {
+    required int bc,
+  }) async => [];
+
+  @override
+  Future<List<BibleRef>> getRef(Database db, {required int bc}) async => [];
+
+  @override
+  Future<List<Verse>> getVersesByIdRange(
+    Database db, {
+    required int fromId,
+    required int? toId,
+  }) async => [];
+
+  @override
+  Future<List<Verse>> search(
+    Database db,
+    String searchText,
+    List<BibleBook> selectedBooks,
+  ) async => [];
 }
 
 class _MemStorage implements Storage {
   final Map<String, dynamic> _data = {};
+
   @override
   Future<void> clear() async => _data.clear();
+
   @override
   Future<void> close() async {}
+
   @override
   Future<void> delete(String key) async => _data.remove(key);
+
   @override
   dynamic read(String key) => _data[key];
+
   @override
   Future<void> write(String key, dynamic value) async => _data[key] = value;
 }
@@ -46,14 +81,19 @@ class _MemStorage implements Storage {
 class _EmptyStore implements InstalledAssetStore {
   @override
   Future<List<String>> listFiles(String directory) async => [];
+
   @override
   Future<Uint8List?> readFile(String relativePath) async => null;
+
   @override
   Future<void> writeFile(String relativePath, Uint8List bytes) async {}
+
   @override
   Future<void> deleteFile(String relativePath) async {}
+
   @override
   Future<bool> exists(String relativePath) async => false;
+
   @override
   Future<void> clear() async {}
 }
